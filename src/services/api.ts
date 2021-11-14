82c862ec939b2b8da19f35e4bdf5dde6dc72572d
Pedro Batista <pmpb742k2@gmail.com>
2021-11-14 23:15:22 +0000

    Added heroku config

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const serverUrl = "http://localhost:4000";
+export const serverUrl = "https://nlw-server-nodejs.herokuapp.com";
 
 export const api = axios.create({
   baseURL: serverUrl,
